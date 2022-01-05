18d267fc02f7e025183accd714f1cc2072ce03e5
Dmitry Kochetkov <dimitry.kochetkov@gmail.com>
2022-01-05 23:03:47 +0200

    fix bug with empty card

--- app/src/main/java/dmk/app/harjumaatransportcardbalance/utils/ResponseUtil.kt
+++ app/src/main/java/dmk/app/harjumaatransportcardbalance/utils/ResponseUtil.kt
@@ -10,10 +10,13 @@ class ResponseUtil {
             val balance = response.getJSONObject("balance")
             val content: JSONArray = balance.getJSONArray("content")
             val contentObject0: JSONObject = content.optJSONObject(0)
-            val contentObject1: JSONObject = response
-                .getJSONObject("tickets")
-                .getJSONArray("content")
-                .optJSONObject(0)
+
+            val contentArray = response.getJSONObject("tickets").getJSONArray("content")
+            if (contentArray.length() == 0) {
+                return "0"
+            }
+
+            val contentObject1: JSONObject = contentArray.optJSONObject(0)
 
             val optString = contentObject1.optString("product_type")
             if (optString.contains("H")) {
