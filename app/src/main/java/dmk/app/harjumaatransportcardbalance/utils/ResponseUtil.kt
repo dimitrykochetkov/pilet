face53106781368f4bc4f15dec17cf9feee59670
Dmitry Kochetkov <dimitry.kochetkov@gmail.com>
2022-03-06 09:13:14 +0200

    fix bug with balance getting

--- app/src/main/java/dmk/app/harjumaatransportcardbalance/utils/ResponseUtil.kt
+++ app/src/main/java/dmk/app/harjumaatransportcardbalance/utils/ResponseUtil.kt
@@ -11,6 +11,11 @@ class ResponseUtil {
             val content: JSONArray = balance.getJSONArray("content")
             val contentObject0: JSONObject = content.optJSONObject(0)
 
+            if (contentObject0.optString("balance").isNotEmpty()) {
+                return contentObject0.optString("balance")
+            }
+
+            // if there is no money and no time-ticket
             val contentArray = response.getJSONObject("tickets").getJSONArray("content")
             if (contentArray.length() == 0) {
                 return "0"
@@ -18,6 +23,7 @@ class ResponseUtil {
 
             val contentObject1: JSONObject = contentArray.optJSONObject(0)
 
+            // time-tickets
             val optString = contentObject1.optString("product_type")
             if (optString.contains("H")) {
                 val start = parseTime(contentObject1.optString("starting"))
@@ -25,7 +31,7 @@ class ResponseUtil {
                 return "$start|$end"
             }
 
-            return contentObject0.optString("balance")
+            return ""
         }
 
         private fun parseTime(time: String): String {
